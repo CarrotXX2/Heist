ea8594cb92a9af25a39f74849bdcb76e3ef53404
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-04-25 15:13:12 +0200

    Pick up.

--- Heist/Assets/Code/PickUp.cs
+++ Heist/Assets/Code/PickUp.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
     public string objectName;
-    public GameObject hand;
-    public float rayLenght;
+    public Transform hand;
+    public float rayLength;
     public RaycastHit hit;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    private bool hasBeenPickedUp = false;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (!hasBeenPickedUp && Input.GetKey(KeyCode.E))
         {
-            if (Physics.Raycast(transform.position, transform.forward, out hit, rayLenght))
+            if (Physics.Raycast(transform.position, transform.forward, out hit, rayLength))
             {
                 if (hit.transform.gameObject.CompareTag("Pickable"))
                 {
-                    Instantiate(GameObject.Find(objectName), hand.transform.position, transform.rotation);
+                    objectName = hit.transform.gameObject.name;
+                    GameObject objectToPickup = GameObject.Find(objectName);
+                    objectToPickup.transform.position = hand.position;
+                    objectToPickup.transform.rotation = hand.rotation;
+                    objectToPickup.transform.SetParent(hand); // Set the parent to the hand
+                    hasBeenPickedUp = true; // Set the flag to true to indicate it has been picked up
                 }
             }
         }
