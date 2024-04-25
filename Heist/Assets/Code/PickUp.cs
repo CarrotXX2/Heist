02900d04ba0d9b6620336b56f7adc7c8d2ce71f0
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-04-25 19:33:18 +0200

    drop (werkt bijna)

--- Heist/Assets/Code/PickUp.cs
+++ Heist/Assets/Code/PickUp.cs
@@ -10,6 +10,7 @@ public class PickUp : MonoBehaviour
     public Transform hand;
     public float rayLength;
     public RaycastHit hit;
+    public bool dropped;
 
     private bool hasBeenPickedUp = false;
 
@@ -26,13 +27,26 @@ public class PickUp : MonoBehaviour
                     objectToPickup.transform.position = hand.position;
                     objectToPickup.transform.rotation = hand.rotation;
                     objectToPickup.transform.SetParent(hand); // Set the parent to the hand
+                    Rigidbody OBJrigid = objectToPickup.GetComponent<Rigidbody>();
+                    OBJrigid.isKinematic = true;
                     hasBeenPickedUp = true; // Set the flag to true to indicate it has been picked up
+                    dropped = false;
                 }
             }
         }
         if (Input.GetKey(KeyCode.G))
         {
-          
+            if (dropped == false)
+            {
+                dropped = true;
+                Instantiate(GameObject.Find(objectName), hand.transform.position, hand.transform.rotation);
+                GameObject objectToDrop = GameObject.Find(objectName);
+                Rigidbody rigidobj = objectToDrop.GetComponent<Rigidbody>();
+                rigidobj.isKinematic = false;
+                hasBeenPickedUp = false;
+                
+            }
+
         }
     }
 }
