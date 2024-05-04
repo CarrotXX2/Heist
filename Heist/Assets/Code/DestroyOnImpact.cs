94e94a3c4ecb7b81f02c0e19f5fc33b9089597ab
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-05-04 14:07:50 +0200

    Glass shatter +effect

--- Heist/Assets/Code/DestroyOnImpact.cs
+++ Heist/Assets/Code/DestroyOnImpact.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class DestroyOnImpact : MonoBehaviour
 {
     public float destroyThreshold = 5f;
-    private ParticleSystem particleSpawn;
-
+    public GameObject particlePrefab; // Assign the particle prefab in the inspector
+    public GameObject glass;
+    public AudioClip soundclip;
 
     public void OnCollisionEnter(Collision collision)
     {
         // Check if the collision velocity magnitude is greater than the threshold
         if (collision.relativeVelocity.magnitude >= destroyThreshold)
         {
-
-            Destroy(gameObject);
-            
+            Instantiate(particlePrefab, transform.position, transform.rotation);
+            StartCoroutine(DestroyGlassAfterDelay(3f));
+            glass.SetActive(false);
+            AudioSource.PlayClipAtPoint(soundclip, transform.position);
         }
+    }
+
+    IEnumerator DestroyGlassAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
     }
 }
