d2a15059fd2b7bf6c967c45579694c524370b6f3
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-05-17 17:00:38 +0200

    buyscreen

--- Heist/Assets/Code/UiCode/Buyscreen.cs
+++ Heist/Assets/Code/UiCode/Buyscreen.cs
@@ -13,9 +13,11 @@ public class Buyscreen : MonoBehaviour
     public float carPrice = 100;
     public float FlashPrice = 100;
     public GameObject car;
+    public GameObject FlashLight;
     // Start is called before the first frame update
     void Start()
     {
+        FlashLight.SetActive(false);
         car.SetActive(false);
     }
 
@@ -27,12 +29,15 @@ public class Buyscreen : MonoBehaviour
     public void carUpgrade()
     {
         car.SetActive(true);
+        FlashLight.SetActive(false);
+        infoHolder.text = "Your bus will have more storage";
         if (itemInBus.money >= carPrice)
         {
             itemInBus.BusUp();
             Debug.Log("Carbought");
             itemInBus.money -= carPrice;
             carPrice *= 2;
+
         }
         else
         {
@@ -43,17 +48,24 @@ public class Buyscreen : MonoBehaviour
     {
         infoHolder.text = "Your bus will have more storage";
         car.SetActive(true);
+        FlashLight.SetActive(false);
     }
     public void Zaklamp()
     {
+        infoHolder.text = "A flashlight so you dont have stumble in the dark";
+        car.SetActive(false);
+        FlashLight.SetActive(true);
         if (!zaklamp.HasBought && itemInBus.money >= FlashPrice)
         {
             zaklamp.HasBought = true;
             itemInBus.money -= carPrice;
+            
         }
     }
     public void InfoZaklamp()
     {
+        FlashLight.SetActive(true);
         infoHolder.text = "A flashlight so you dont have stumble in the dark";
+        car.SetActive(false); 
     }
 }
