9682d87af8e8950c84858b6b36638ae178d55fde
CarrotXX2 <silver.bgame@gmail.com>
2024-05-22 11:29:10 +0200

    UIFix

---
+++ Heist/Assets/Code/UiCode/SceneLoader2.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader2 : MonoBehaviour
+{
+    public void playclicked()
+    {
+        SceneManager.LoadScene("Buy Menu1");
+
+    }
+}
