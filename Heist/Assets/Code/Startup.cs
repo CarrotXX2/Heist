d12b4cb0290f4e4f1e1167fbb991407bc3ade827
CarrotXX2 <silver.bgame@gmail.com>
2024-06-17 02:39:46 +0200

    Terrain change en startup change

--- Heist/Assets/Code/Startup.cs
+++ Heist/Assets/Code/Startup.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Startup : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float fadeDuration = 5.0f; 
+    public Image image; 
+
+    private Color originalColor;
+
     void Start()
     {
-        
+        if (image == null)
+        {
+            return;
+        }
+
+        originalColor = image.color;
+        StartCoroutine(FadeOut());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FadeOut()
     {
-        
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            image.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            yield return null;
+        }
+
+        image.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0f);
     }
 }
