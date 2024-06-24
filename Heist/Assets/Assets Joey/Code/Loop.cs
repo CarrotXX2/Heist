e7dcd6d1bae4f3b1707ff50910e8d15e0d681818
joeyproxxl <68194340+joeyproxxl@users.noreply.github.com>
2024-06-24 12:35:42 +0200

    robert animation error fix

---
+++ Heist/Assets/Assets Joey/Code/Loop.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop : MonoBehaviour
+{
+    public float looptime;
+    public float stoptime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void walking()
+    {
+        
+    }
+}
