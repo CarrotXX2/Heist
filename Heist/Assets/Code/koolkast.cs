441c54be3c078fd383749c33f7ea2e5a6138b6ee
CarrotXX2 <silver.bgame@gmail.com>
2024-06-25 13:02:59 +0200

    Koolkast ka nopen

--- Heist/Assets/Code/koolkast.cs
+++ Heist/Assets/Code/koolkast.cs
@@ -1,52 +1,44 @@
 using UnityEngine;
-using TMPro;
-using System.Collections;
 
 public class Koolkast : MonoBehaviour
 {
     public Transform objectToRotate;  // Het object dat moet draaien
     public float rotationSpeed = 1f;  // Snelheid van de rotatie
-    public bool isLookingAtFridge;
-    public Transform fridge;    
+    public Transform fridge;
 
+    private bool isOpening = false;
 
     void Update()
     {
         // Als de sleutel is opgepakt, draai het object als je naar de "Garage" kijkt
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, transform.forward, out hit, 15))
             {
                 if (hit.collider.gameObject.CompareTag("Fridge"))
                 {
-                    RotateObject();
+                    isOpening = true;
                 }
             }
         }
-    }
 
-    bool IsLookingAtFridge()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
+        if (isOpening)
         {
-            if (hit.transform == fridge.transform)
-            {
-                return true;
-            }
+            RotateObject();
         }
-        return false;
     }
-
-
-
 
     void RotateObject()
     {
         Quaternion targetRotation = Quaternion.Euler(0, -166, 0); // Roteer naar 90 graden op de Z-as
         objectToRotate.rotation = Quaternion.Slerp(objectToRotate.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+
+        // Stop rotating once the object is close enough to the target rotation
+        if (Quaternion.Angle(objectToRotate.rotation, targetRotation) < 0.1f)
+        {
+            objectToRotate.rotation = targetRotation; // Snap to the target rotation
+            isOpening = false; // Stop rotating
+        }
     }
 }
