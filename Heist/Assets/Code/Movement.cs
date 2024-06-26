a1947b657c32a608e6a276289e2aeed9c48e5e63
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-06-26 13:57:38 +0200

    idle/walk sway

--- Heist/Assets/Code/Movement.cs
+++ Heist/Assets/Code/Movement.cs
@@ -13,11 +13,23 @@ public class MoveMent : MonoBehaviour
     private Rigidbody rb;
     public float jumpforce = 6f;
     public bool isSprinting;
+
+    public float swayAmount = 0.05f;
+    public float swaySpeed = 5f;
+    public float idleSwayAmount = 0.01f;
+    public float idleSwaySpeed = 1f;
+    private Vector3 initialCameraPosition;
+    public Camera playerCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-            Cursor.lockState = CursorLockMode.Locked;
-            rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        if (playerCamera != null)
+        {
+            initialCameraPosition = playerCamera.transform.localPosition;
+        }
     }
 
     // Update is called once per frame
@@ -27,27 +39,40 @@ public class MoveMent : MonoBehaviour
         hor = Input.GetAxis("Horizontal");
         movedir.z = vert;
         movedir.x = hor;
-        {
-            isSprinting = Input.GetKey(KeyCode.LeftShift);
-            transform.Translate(movedir * Time.deltaTime * (isSprinting ? sprintspeed : walkspeed));
-        }
+
+        isSprinting = Input.GetKey(KeyCode.LeftShift);
+        transform.Translate(movedir * Time.deltaTime * (isSprinting ? sprintspeed : walkspeed));
 
         if (Input.GetButtonDown("Jump"))
         {
             Jump();
         }
+
+        if (playerCamera != null)
+        {
+            ApplyCameraSway();
+        }
     }
+
     void Jump()
     {
         if (Mathf.Abs(rb.velocity.y) < 0.001f)
         {
             rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
-            if (Mathf.Abs(rb.velocity.y) < 0.001f)
-            {
-                rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
-            }
         }
     }
+
+    void ApplyCameraSway()
+    {
+        bool isMoving = vert != 0 || hor != 0;
+        float currentSwayAmount = isMoving ? swayAmount : idleSwayAmount;
+        float currentSwaySpeed = isMoving ? swaySpeed : idleSwaySpeed;
+
+        float swayX = Mathf.Sin(Time.time * currentSwaySpeed) * currentSwayAmount;
+        float swayY = Mathf.Cos(Time.time * currentSwaySpeed) * currentSwayAmount;
+        playerCamera.transform.localPosition = initialCameraPosition + new Vector3(swayX, swayY, 0);
+    }
+
     public void death()
     {
         walkspeed = 0;
